7cbcfe485c8613afa7c1972d5bc7aedde4a53fb8
BalavigneshRajasekar <you@example.com>
2024-04-30 23:58:12 +0530

    New feature added

--- Rate-card/src/App.jsx
+++ Rate-card/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let div = useRef();
+  function checked(e) {
+    e.target.checked
+      ? div.current.classList.replace("d-none", "d-block")
+      : div.current.classList.replace("d-block", "d-none");
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container border border-1 border-danger">
+      <input type="text" className="form-control mt-5"></input>
+      <button className="btn btn-primary mt-4">Add User</button>
+      <input type="checkbox" onClick={checked}></input>
+      <div className="d-none" ref={div}>
+        black
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
