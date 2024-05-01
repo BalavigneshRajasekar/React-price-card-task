15fd7b47b05b8f349e3e6a73ddaa8f01bcd95bcf
BalavigneshRajasekar <you@example.com>
2024-05-01 22:28:42 +0530

    The card Template is  created and fetched data from props

---
+++ Rate-card/src/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function Card(props) {
+  // Destructuring the props
+  let { title, tick, price, user, xMark, dummy, style, style1 } = props;
+
+  // This Return the Rate card HTML element
+  return (
+    <div id="Card">
+      <div className="card-body ">
+        <p className="text-center fw-bolder pt-2">{title}</p>
+        <h1 className="text-center border-bottom border-secondary pb-4">
+          ${price}/month
+        </h1>
+        <ul id="ul" className="list-group">
+          <li>
+            {tick}
+            {user}
+          </li>
+          <li>{tick}50GB Storage</li>
+          <li>{tick}Unlimited Public Project</li>
+          <li>{tick}Community Access</li>
+          <li className={style}>{xMark}Unlimited Private Project</li>
+          <li className={style}>{xMark}Dedicated Phone Support</li>
+          <li className={style}>{xMark}Free Subdomain</li>
+          <li className={style1}>{dummy}Monthly Status Report</li>
+        </ul>
+        <button className="btn btn-primary px-5  mt-3 ">button</button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
